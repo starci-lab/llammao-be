a14bd7e0ec5497205c3ca894984d9e3be8f1dafe
starci183 <cuongnvtse160875@gmail.com>
2025-07-30 22:51:20 +0700

    fux

--- src/jwt.strategy.ts
+++ src/jwt.strategy.ts
@@ -13,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     async validate(payload: any) {
-        return { userId: payload.sub, address: payload.userAddress }
+        return payload
     }
 }
 
